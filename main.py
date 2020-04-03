5a21abcb4d44ddf2a65af00c79fcb844f3ec6716
alexanderzou <azou00@stuy.edu>
2020-04-03 14:11:35 -0400

    own pic

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ ident(t)
 csystems = [ t ]
 
 parse_file( 'script', edges, polygons, csystems, screen, color )
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+polygons = []
+t = new_matrix()
+ident(t)
+csystems = [ t ]
+
+parse_file( 'chain', edges, polygons, csystems, screen, color )
